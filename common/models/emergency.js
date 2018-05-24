42ea35316e217fd59cbef67202c2bad4cfab2aa7
Brandon <me@brandoncodes.com>
2018-05-24 12:44:25 -0400

    Fixes to alerts

--- common/models/emergency.js
+++ common/models/emergency.js
@@ -25,14 +25,16 @@ module.exports = function(Emergency) {
     if (ctx.isNewInstance) {
       console.log('Sending Alert');
       var Sub = Emergency.app.models.Subscription;
-      Sub.find({where: {'responseArea': ctx.instance.responseAreaId}},
-        function result(err, obj) {
+      console.log('Finding Subscriptions: ' + ctx.instance.responseAreaId);
 
+      Sub.find({where: {responseAreaId: ctx.instance.responseAreaId}},
+        function result(err, subscriptions) {
+          console.log(subscriptions);
           var Usr = Emergency.app.models.Responder;
           var Sender = Emergency.app.models.Twilio;
 
-          for (var i = 0; i < obj.length; i++) {
-            Usr.findOne({where: {'id': obj[i].responderId}},
+          for (var i = 0; i < subscriptions.length; i++) {
+            Usr.findOne({where: {id: subscriptions[i].responderId}},
               function sendAlert(err, usr) {
                 var msg = 'Emergency Event: ' + ctx.instance.type +
                   ' https://era.brandoncodes.com/events/' + ctx.instance.id;
@@ -44,17 +46,40 @@ module.exports = function(Emergency) {
                       emergencyId: ctx.instance.id,
                       status: 'Failed'});
                 } else {
-                  Response.create(
-                    {responderId: usr.id,
-                      emergencyId: ctx.instance.id,
-                      status: 'No Response'});
-
                   if (process.env.SEND_ALERTS === 'ACTIVE') {
-                    Sender.send({type: 'sms', to: '+1' + obj.cellNumber,
+                    console.log(usr);
+                    Sender.send({type: 'sms', to: '+1' + usr.cellNumber,
                       from: '+18592872026', body: msg}, function(err, msg) {
+                      if (err) {
+                        console.log(err);
+                        Response.create(
+                          {responderId: usr.id,
+                            emergencyId: ctx.instance.id,
+                            status: 'Failed'},
+                          function(err, msg) {
+                            if (err) {
+                              console.log(err);
+                            } else {
+                              console.log(msg);
+                            }
+                          });
+                      } else {
+                        Response.create(
+                          {responderId: usr.id,
+                            emergencyId: ctx.instance.id,
+                            status: 'No Response'},
+                          function(err, msg) {
+                            if (err) {
+                              console.log(err);
+                            } else {
+                              console.log(msg);
+                            }
+                          });
+                        console.log(msg);
+                      }
                     });
                   } else {
-                    console.log('Test Sending Alert To: ' + obj.cellNumber);
+                    console.log('Test Sending Alert To: ' + usr.cellNumber);
                   }
                 }
               });
